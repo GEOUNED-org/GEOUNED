bc287223ee5566c595b7ee0467b1302226d39ae3
Jonathan Shimwell <mail@jshimwell.com>
2024-04-25 01:19:04 +0100

    removed spare ]

--- src/geouned/GEOUNED/LoadFile/LoadSTEP.py
+++ src/geouned/GEOUNED/LoadFile/LoadSTEP.py
@@ -14,7 +14,7 @@ from . import LoadFunctions as LF
 
 # Paco mod
 def extractMaterials(filename):
-    rhoreal = []]
+    rhoreal = []
     mdict = {}  # _ Material dictionary
     with open(filename, "rt") as file:
         for line in file:
