ffc0a0937f65169297d3e1ad374a4de27e59db82
Jonathan Shimwell <mail@jshimwell.com>
2024-05-07 16:23:50 +0100

    added arg parse script

---
+++ src/geouned/GEOUNED/scripts/geouned_CadToCsg.py
@@ -0,0 +1,6 @@
+
+def main():
+    print('hellow w')
+
+if __name__ == "__main__":
+    main()
---
+++ src/geouned/GEOUNED/scripts/geouned_cadtocsg.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Convert Cad geometry to CSG format for use in Monte Carlo Codes
+"""
+
+import geouned
+import argparse
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument(
+    "-i",
+    "--input",
+    type=str,
+    default="config.json",
+    help="The path to the config JSON/JSON5 file",
+)
+args = parser.parse_args()
+
+def main():
+    geouned.CadToCsg.from_config(args.input)
+
+if __name__ == "__main__":
+    main()
