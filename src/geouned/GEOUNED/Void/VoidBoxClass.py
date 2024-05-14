2dfe18feb108c3f28a5b9312fe6889d482579db9
Jonathan Shimwell <mail@jshimwell.com>
2024-05-14 14:26:22 +0100

    format

--- src/geouned/GEOUNED/Void/VoidBoxClass.py
+++ src/geouned/GEOUNED/Void/VoidBoxClass.py
@@ -205,7 +205,10 @@ class VoidBox:
                     tolerances,
                     numeric_format,
                     MakeObj=True,
-                ), options, tolerances, numeric_format
+                ),
+                options,
+                tolerances,
+                numeric_format,
             )
             TempPieceEnclosure.update_solids(comsolid.Solids)
             Conv.cellDef(
