49a22e3c977d71927e820e5ca0cce12f660843dd
Jonathan Shimwell <mail@jshimwell.com>
2024-05-24 21:05:57 +0100

    format

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ author = "Juan-Pablo Catalan and Patrick Sauvan"
 
 # The full version, including alpha/beta/rc tags
 import geouned
+
 version = geouned.__version__
 release = geouned.__version__
 
