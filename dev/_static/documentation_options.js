0c6c001aa615eece1501bc010db4a6e53b63e7d2
shimwell <mail@jshimwell.com>
2024-11-25 18:58:04 +0100

    cleaned up root gh pages folder added redirect

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,5 +1,9 @@
 const DOCUMENTATION_OPTIONS = {
+<<<<<<< HEAD
     VERSION: '0.1.dev1+g629ec26',
+=======
+    VERSION: '0.1.dev1+g8cccf75',
+>>>>>>> c49764ffd70af4130a054f55377f4fa1ce094b9f
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
