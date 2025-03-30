8229eec5a53c6667674c74a47380514a6715f579
Patrick Sauvan <psauvan@ind.uned.es>
2025-03-30 23:38:53 +0200

    change CI CsgToCad script

--- tests/test_csgtocad.py
+++ tests/test_csgtocad.py
@@ -13,16 +13,14 @@ def test_cylbox_convertion(csg_format):
 
     geo = geouned.CsgToCad()
 
-    geo.export_cad(
+    geo.read_csg_file(        
         # csg file was made from testing/inputSTEP/cylBox.stp
         input_filename=f"tests/csg_files/cylinder_box{suffix}",
         csg_format=csg_format,
-        bounding_box=[-1000.0, -500.0, -1000.0, 0, 0, 0.0],
-        # TODO add tests for these args that counts volumes in cad file
-        # cell_range_type='exclude',
-        # cell_range=(2,3,4),
-        output_filename=f"tests_outputs/csgtocad/{csg_format}",
-    )
+        )
+    
+    geo.build_universe()
+    geo.export_cad(output_filename=f"tests_outputs/csgtocad/{csg_format}")
 
-    assert Path(f"tests_outputs/csgtocad/{csg_format}.step").exists()
+    assert Path(f"tests_outputs/csgtocad/{csg_format}.stp").exists()
     assert Path(f"tests_outputs/csgtocad/{csg_format}.FCStd").exists()
