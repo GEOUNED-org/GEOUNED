0aeca12751c4dc4322f20649e6461b15747db067
Patrick Sauvan <psauvan@ind.uned.es>
2025-03-30 23:40:02 +0200

    black format

--- tests/test_csgtocad.py
+++ tests/test_csgtocad.py
@@ -13,12 +13,12 @@ def test_cylbox_convertion(csg_format):
 
     geo = geouned.CsgToCad()
 
-    geo.read_csg_file(        
+    geo.read_csg_file(
         # csg file was made from testing/inputSTEP/cylBox.stp
         input_filename=f"tests/csg_files/cylinder_box{suffix}",
         csg_format=csg_format,
-        )
-    
+    )
+
     geo.build_universe()
     geo.export_cad(output_filename=f"tests_outputs/csgtocad/{csg_format}")
 
